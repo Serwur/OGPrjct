19c37033408d301b5ad7f7fd25057704b4e03484
Sharpshark <maciej.jasielewicz@gmail.com>
2019-05-05 03:52:44 +0200

    Added Snapping and Enhanced Interaface
    Added script "DraggableItem" to drag items in inventory

---
+++ Assets/Scripts/UI/DraggableItem.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+
+    public float snapToSlotConstant = 120f; //distance 
+    public string nameOfInventoryContainer = "Inventory";
+    public string nameOfAttachedInventoryContainer = "Sword Inventory";
+
+    Transform slotsOfInventory, slotsOfAttachedInvenotry; //slots in inventory
+    List<Transform> allSlots = new List<Transform>(); //List of all slots
+    float smallestDistance = 1000f;
+
+    
+
+    public void Start()
+    {
+        slotsOfInventory = GameObject.Find(nameOfInventoryContainer).transform;
+        slotsOfAttachedInvenotry = GameObject.Find(nameOfAttachedInventoryContainer).transform;
+
+        foreach(Transform slot in slotsOfInventory)
+        {
+            allSlots.Add(slot);
+        }
+        foreach(Transform slot in slotsOfAttachedInvenotry)
+        {
+            allSlots.Add(slot);
+        }
+
+
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+
+        foreach (Transform slot in allSlots)
+        {
+            smallestDistance = Vector2.Distance(this.transform.position, slot.position);
+            if (smallestDistance <= snapToSlotConstant) //looks for a slot that is the nearest
+            {
+                this.transform.position = slot.position; //snaps to a slot
+            }
+            else //comes back to slot that was from
+            {
+
+            }
+
+        }
+
+    }
+}
