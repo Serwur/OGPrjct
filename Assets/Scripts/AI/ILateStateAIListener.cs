afc19e8c5865bb32fa6b2d2ccb9e1ae4e5b48586
Serwur <lychaserwur@gmail.com>
2019-09-19 22:21:54 +0200

    AI Interfaces for listining

---
+++ Assets/Scripts/AI/ILateStateAIListener.cs
@@ -0,0 +1,19 @@
+﻿namespace ColdCry.AI
+{
+    public interface ILateStateAIListener
+    {
+        void SetLateSleepState(AIMovementStatus oldState);
+        void SetLateWatchState(AIMovementStatus oldState);
+        void SetLateReachState(AIMovementStatus oldState);
+        void SetLateAttackState(AIMovementStatus oldState);
+        void SetLateFollowState(AIMovementStatus oldState);
+        void OnAnyLateStateChange(AIMovementStatus oldState, AIMovementStatus newState);
+
+        void LateSleepUpdate();
+        void LateWatchUpdate();
+        void LateReachUpdate();
+        void LateFollowUpdate();
+        void LateAttackUpdate();
+        void OnAnyLateStateUpdate(AIMovementStatus currentState);
+    }
+}
---
+++ Assets/Scripts/AI/IStateAIListener.cs
@@ -0,0 +1,19 @@
+﻿namespace ColdCry.AI
+{
+    public interface IStateAIListener
+    {
+        void SetSleepState(AIMovementStatus oldState);
+        void SetWatchState(AIMovementStatus oldState);
+        void SetReachState(AIMovementStatus oldState);
+        void SetAttackState(AIMovementStatus oldState);
+        void SetFollowState(AIMovementStatus oldState);
+        void OnAnyStateChange(AIMovementStatus oldState, AIMovementStatus newState);
+
+        void SleepUpdate();
+        void WatchUpdate();
+        void ReachUpdate();
+        void FollowUpdate();
+        void AttackUpdate();
+        void OnAnyStateUpdate(AIMovementStatus currentState);
+    }
+}
